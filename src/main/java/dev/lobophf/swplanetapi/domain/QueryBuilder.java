83e488d1af46116a451df13cc685a7789e0afbcd
loboph <lobophf@gmail.com>
2022-09-28 19:33:37 -0300

    add test report with Jacoco

--- src/main/java/dev/lobophf/swplanetapi/domain/QueryBuilder.java
+++ src/main/java/dev/lobophf/swplanetapi/domain/QueryBuilder.java
@@ -4,8 +4,11 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
 public class QueryBuilder {
-  public static Example<Planet> makeQuery(Planet planet) {
-    ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
-    return Example.of(planet, exampleMatcher);
-  }
+	private QueryBuilder() {
+	}
+
+	public static Example<Planet> makeQuery(Planet planet) {
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
+		return Example.of(planet, exampleMatcher);
+	}
 }
